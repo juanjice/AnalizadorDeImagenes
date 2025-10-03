b6665771f3626be72f6326a5f0b7c960560e2fd1
juanjice <jimenezcelisj2@gmail.com>
2025-10-03 16:29:21 -0500

    se añade configuracion de s3 local 'minio' y configuracion bean S3

--- backend/image-analyzer/src/main/java/com/example/image_analyzer/config/S3Config.java
+++ backend/image-analyzer/src/main/java/com/example/image_analyzer/config/S3Config.java
@@ -1,4 +1,45 @@
 package com.example.image_analyzer.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.S3Configuration;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+
+import java.net.URI;
+
 public class S3Config {
+
+    @Value("${s3.endpoint}")
+    private String endpoint;
+    @Value("${s3.access-key}")
+    private String accessKey;
+    @Value("${s3.secret-key}")
+    private String secretKey;
+    @Value("${s3.region}")
+    private String region;
+    @Value("${s3.bucket}")
+
+    private String bucketName;
+    @Bean
+    public S3Client s3Client() {
+        return S3Client.builder()
+                .endpointOverride(URI.create(endpoint))
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+                .serviceConfiguration(S3Configuration.builder().pathStyleAccessEnabled(true).build())
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
+                .endpointOverride(URI.create(endpoint))
+                .region(Region.of(region))
+                .credentialsProvider(StaticCredentialsProvider.create(AwsBasicCredentials.create(accessKey, secretKey)))
+                .build();
+    }
 }
--- backend/image-analyzer/src/main/java/com/example/image_analyzer/service/ImageService.java
+++ backend/image-analyzer/src/main/java/com/example/image_analyzer/service/ImageService.java
@@ -5,12 +5,15 @@ import com.example.image_analyzer.repository.ImageRecordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @Service
 public class ImageService {
 
     @Autowired
     private ImageRecordRepository imageRecordRepository;
+    @Autowired
+    private S3Client s3Client;
 
     public ImageRecord submitForAnalysis(MultipartFile file){
         return null;
